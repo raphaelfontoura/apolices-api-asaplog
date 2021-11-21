0bca259a50919cf8444d74920e40b8af446d9678
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-11-21 14:34:30 -0300

    first commit

---
+++ backend/src/main/java/br/com/asaplog/veiculoseguro/VeiculoSeguroApplication.java
@@ -0,0 +1,13 @@
+package br.com.asaplog.veiculoseguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculoSeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculoSeguroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/asaplog/veiculoseguro/VeiculoSeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.asaplog.veiculoseguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculoSeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
